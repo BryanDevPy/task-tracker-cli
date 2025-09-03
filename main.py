a32b4f204f0e91df9ed4c9f6afae4366f3624a45
Brayan Ellis <ellis@localhost.localdomain>
2025-09-03 16:49:36 -0400

    feat: Initialize task manager structure with placeholder functions

---
+++ main.py
@@ -0,0 +1,20 @@
+def add_task():
+    pass
+
+def update_task(id):
+    pass
+
+def delete_task(id):
+    pass
+
+def mark_task(arg, id):
+    pass
+
+def list_task(arg):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
