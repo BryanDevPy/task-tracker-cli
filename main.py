679d31d3d419c6c4ca04a3e3c0c4ac37f770569d
Brayan Ellis <ellis@localhost.localdomain>
2025-09-05 19:01:42 -0400

    Agrego soporte para .json
    - Importó json y Path
    - Agrego "agenda.json" como Path
    - Agrego la función load_agenda que carga la agenda desde el archivo json
    - Modifico la función add_task, que ahora guarda las tareas directamente en el archivo json

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-all_task = {
-    '1': {
-        'description':'Task Test 1',
-        'status':'todo',
-        'createdAt':True,
-        'updatedAt':False
-    },
-    '2': {
-        'description':'Task Test 2',
-        'status':'in-progress',
-        'createdAt':True,
-        'updatedAt':True
-    },
-}
+import json
+from pathlib import Path
+
+AGENDA_FILE = Path('agenda.json')
+
+def load_agenda():
+    """
+    Carga la agenda desde el archivo JSON.
+    - Si el archivo existe, devuelve el diccionario de contactos.
+    - Si no existe, devuelve un diccionario vacío {}
+    - Si el archivo está corrupto (ej: contenido inválido), también devuelve {}.
+    """
+    if AGENDA_FILE.exists():
+        try:
+            with open(AGENDA_FILE, 'r', encoding='utf-8') as f:
+                return json.load(f) # -> {{}}
+        except json.JSONDecodeError:
+            # Archivo corructo o vacío: arrancamos con un diccionario vacío
+            return {}
+    return {}
+
 def get_id(agenda: dict) -> str:
     """ Generates a new incremental ID based on the calendar keys.
     Args:
@@ -23,14 +30,32 @@ def get_id(agenda: dict) -> str:
     return str(new_id)
 
 def add_task(description: str):
-    task_id = get_id(all_task)
-    new_task = {
+    """ Agrega una nueva tarea a la agenda.
+
+    La función carga la agenda desde el archivo JSON,
+    genera un nuevo ID para la tarea y la guarda 
+    con estado "todo"
+
+    Args:
+        description (str): Descripción de la tarea.
+
+    Returns: None
+    """
+    task = load_agenda() # Cargamos el dict actual
+    task_id = get_id(task) # Obtenemos el id disponible
+
+    content_task = {
         'description':description,
         'status':'todo',
         'createdAt':True,
         'updatedAt':False
     }
-    all_task[task_id] = new_task
+
+    task[task_id] = content_task # Insertamos la nueva tarea
+
+    # Persistimos la agenda actualizada en el archivo
+    with open(AGENDA_FILE, 'w', encoding='utf-8') as f:
+        json.dump(task, f, ensure_ascii=False, indent=4)
 
 def update_task(id):
     pass
@@ -54,7 +79,6 @@ def list_task():
 
 def main():
     add_task('descripción para test')
-    list_task()
 
 if __name__ == '__main__':
     main()
